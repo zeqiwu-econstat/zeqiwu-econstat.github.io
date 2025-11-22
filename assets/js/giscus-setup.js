af1387ad74fc52ab6fd39ada7d8b226c0bafc5ab
zeqiwu1202 <wuzeqi@ruc.edu.cn>
2025-11-22 05:14:17 +0000

    Deploying to gh-pages from @ zeqiwu1202/zeqiwu1202.github.io@c0ffcafbbaae42e07deff62ac8a6d09fe0f624d1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zeqiwu-econstat/zeqiwu.github.io",
+    "data-repo": "zeqiwu1202/zeqiwu1202.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
